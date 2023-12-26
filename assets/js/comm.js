5684a355fc2146524ab217226616820c977c03a0
김초향 <chkim9910@naver.com>
2023-12-26 18:13:00 +0900

    23/12/26

--- assets/js/comm.js
+++ assets/js/comm.js
@@ -32,7 +32,7 @@ var swiper = new Swiper(".thumb-tit", {
 var swiper2 = new Swiper(".swiper-main-activities", {
   loop: true,
   spaceBetween: 10,
-  slidesPerView: "auto",
+  // slidesPerView: "auto",
   centeredSlides: true,
   navigation: {
     nextEl: ".swiper-button-next",
